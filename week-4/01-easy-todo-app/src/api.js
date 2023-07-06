8b5815eb91038aa3c9d71f2194ee8332ce88bf8c
Sai Kumar P B <50828572+saikumarpb@users.noreply.github.com>
2023-07-06 07:09:15 +0530

    Solve week-4/01-easy-todo-app (#5)

---
+++ week-4/01-easy-todo-app/src/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { BACKEND_URL } from './config';
+
+export const fetchTodoList = async () => {
+  const res = await axios.get(`${BACKEND_URL}/todos`);
+  return res;
+};
+
+export const postTodo = async ({ title, description }) => {
+  const res = await axios.post(
+    `${BACKEND_URL}/todos`,
+    { title: title, description: description },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+  return res;
+};
+
+export const updateTodo = async ({ id, title, description }) => {
+  const res = await axios.put(
+    `${BACKEND_URL}/todos/${id}`,
+    { title, description },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  return res;
+};
+
+export const deleteTodo = async (id) => {
+  const res = await axios.delete(`${BACKEND_URL}/todos/${id}`);
+  return res;
+};
